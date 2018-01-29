0c7984f3ffb3263a629657a323446b6eeb6cde77
Colton <16970228+nirinium@users.noreply.github.com>
2018-01-29 16:04:32 -0500

    Changes
    1/29/2018

--- STIGPY3/STIGPY3dev.py
+++ STIGPY3/STIGPY3dev.py
@@ -1,11 +1,12 @@
 import os
 import sys
+import time
 import logging
 from bs4 import BeautifulSoup
 # PYTHON 3.X
 from urllib.request import urlopen, urlretrieve
 
-URL = 'https://iase.disa.mil/stigs/Pages/a-z.aspx'
+URL = 'https://iase.disa.mil/stigs/Pages/a-z.aspx?Paged=TRUE&p_Title=Microsoft%20Windows%202012%20Server%20DNS%20STIG%20-%20Ver%201%2c%20Rel%207%20&p_ID=813&PageFirstRow=301&&View={25A09AF8-178B-447B-B42B-8839EBD71CAD}'
 OUTPUT_DIR = 'stigs'
 
 #START
@@ -14,17 +15,17 @@ print("Grabbing STIGs...")
 #PROGRESS PERCENTAGE
 def dlProgress(count, blockSize, totalSize):
       percent = int(count*blockSize*100/totalSize)
-      filesize = str(totalSize) # Unused right now... will be implemented later!
-      sys.stdout.write("\r" + "...%d%% "  % percent)
-      sys.stdout.flush
+      sys.stdout.write("\r" + "...%d%% " % percent)
+      sys.stdout.flush()
 
 u = urlopen(URL)
+f = open('logging.txt', 'w')
 try:
     html = u.read().decode('utf-8')
 finally:
     u.close()
 
-soup = BeautifulSoup(html, "lxml")
+soup = BeautifulSoup(html, "html.parser")
 for link in soup.select('a[href^="http://"]'):
     href = link.get('href')
     if not any(href.endswith(x) for x in ['.zip', '.xls', '.xlsx', '.csv', '.ppt', '.pptx', '.pdf', '.docx', '.doc', '.txt']):
@@ -37,6 +38,4 @@ for link in soup.select('a[href^="http://"]'):
     
     print("> %s to %s..." % (href, filename) )
     urlretrieve(href, filename, reporthook = dlProgress)
-    print("DONE!")
-
-
+    print("Done")
